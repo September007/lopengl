a0a15cff44e14577a3b791130a48d5c9f73c9c53
xucl <2868852383@qq.com>
2022-08-09 16:15:54 +0800

    00.hello-window

---
+++ src/01.hellow-triangle.cpp
@@ -0,0 +1,50 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow*window){
+    if(glfwGetKey(window,GLFW_KEY_ESCAPE)==GLFW_PRESS){
+        glfwSetWindowShouldClose(window,true);
+    }
+}
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    auto window = glfwCreateWindow(800, 600, "Learning OpenGL", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "create widnow failed" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "glad load failed" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window)){
+        processInput(window);
+        glfwPollEvents();
+
+        glClearColor(0.2,.2,.3,1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwSwapBuffers(window);
+    }
+    return 0;
+}
