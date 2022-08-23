89f938e354c2a2007ea6fdabf6bbc105a1ed1f52
xucl <xucl@300624.cn>
2022-08-23 14:52:05 +0800

    1. add ImGUI_Util  type constraint 2. add ImGUI_Util  Draw Element

---
+++ src/include/ImGUI/ImGUI_Utils.h
@@ -0,0 +1,155 @@
+#pragma once
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+#include <stdio.h>
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+// clang-format off
+#include <concepts>
+#include <type_traits>
+#include <tuple>
+#include <string>
+template <typename... T>
+struct is_from_tuple : public std::false_type{};
+template <typename... T>
+struct is_from_tuple<std::tuple<T...>> : public std::true_type{};
+template <typename... T>
+constexpr bool is_from_tuple_v = is_from_tuple<T...>::value;
+
+// constraint for type to be use as attr to show
+// show in DragInt or DragFloat
+template <typename T>
+concept Visible_Attr_Type = requires(T t)
+{
+    t.GetName();
+    t.GetMin();
+    t.GetMax();
+    t.GetSpeed();
+    t.GetFormat();
+    t.GetFlag();
+}
+&&std::same_as<std::string, decltype(std::declval<T>().GetName())>;
+
+// constraint for type to be use as attr group to show
+// show in CollapsingHeader
+template <typename T>
+concept Visible_Attr_Group_Type=requires(T t){
+    // reflection
+    t.GetName();
+    t.GetAllAttr();
+};
+template<typename T>
+concept Qualified_Be_Wrapped=requires(T t){
+    t.GetAllAttr();
+};
+// note:
+// if you declare T as non-reference,then this will hold data as itself
+// otherwise, the Universal_Type_Wrapper hold a reference to origin data
+template <typename T>
+requires( std::integral<std::remove_reference_t<T>>||
+          std::floating_point<std::remove_reference_t<T>>)
+ struct Universal_Type_Wrapper
+{
+    using ValueType=T;
+    using DT=std::remove_reference_t<ValueType>;
+    static constexpr float int_speed=0.0000001;
+    static constexpr float float_speed=0.005;
+    
+    std::string name;
+    T data;
+    DT v_min,v_max;
+    ImGuiSliderFlags_ flag;
+    float speed;
+    Universal_Type_Wrapper(const std::string &name,  T data, DT v_min=std::numeric_limits<T>::min(), DT v_max=std::numeric_limits<T>::max(),float speed=GetDefaultSpeed() ,ImGuiSliderFlags_ flag=ImGuiSliderFlags_AlwaysClamp)
+     :name(name), data(data),v_min(v_min),v_max(v_max),flag(flag){};
+
+    auto GetName() { return name; }
+   // dont need this
+   // auto GetAttr() { return std::make_tuple(std::make_pair(name, &data)); }
+    auto GetMin() { return v_min; }
+    auto GetMax() { return v_max; }
+    auto GetSpeed() { return speed;}
+    auto GetFormat()
+    {
+        if constexpr (std::is_integral_v<DT>)
+            return "%lld";
+        else if constexpr (std::is_floating_point_v<DT>)
+            return "%lf";
+       else if constexpr (std::is_same_v<T,T>)
+           static_assert(!std::is_same_v<T,T>,"Unsupport Type");
+    }
+    auto GetFlag(){ return flag;}
+
+   private:
+    static auto constexpr GetDefaultSpeed(){
+        if constexpr (std::is_integral_v<DT>)
+            return int_speed;
+        else if constexpr (std::is_floating_point_v<DT>)
+            return float_speed;
+       else if constexpr (std::is_same_v<T,T>)
+           static_assert(!std::is_same_v<T,T>,"Unsupport Type");
+    }
+};
+// note if type T is qualified to be wrapped,
+// it need implement GetAllAttr() 
+template<typename T>
+requires std::is_class_v<T>
+struct Universal_Group_Wrapper{
+    using ValueType=T;
+    T data;
+    std::string name;
+    ImGuiTreeNodeFlags_ flag;
+    Universal_Group_Wrapper(const std::string&name,T data,
+    ImGuiTreeNodeFlags_ flag=ImGuiTreeNodeFlags_::ImGuiTreeNodeFlags_DefaultOpen|ImGuiTreeNodeFlags_::ImGuiTreeNodeFlags_DefaultOpen):
+    name(name),data(data),flag(flag){}
+    auto GetName() { return name; }
+    auto GetFlag(){ return flag; }
+    auto GetAllAttr()->decltype( data.GetAttr()){
+        return data.GetAllAttr();
+    }
+};
+// clang-format on
+template <Visible_Attr_Type T = Universal_Type_Wrapper<int>>
+inline void Draw_element(T &t)
+{
+    using RRT=std::remove_reference_t<typename T::ValueType>;
+    auto &data = t.data;
+    if constexpr (std::is_same_v<RRT, float>)
+    {
+        ImGui::DragFloat(t.GetName().c_str(), &data, 0.005, t.GetMin(), t.GetMax(), t.GetFormat(), t.GetFlag());
+    }
+    else if constexpr (std::is_same_v<RRT, int>)
+    {
+        ImGui::DragInt(t.GetName().c_str(), &data, t.GetSpeed(), t.GetMin(), t.GetMax(), t.GetFormat(), t.GetFlag());
+    }else if constexpr(std::is_same_v<T,T>){
+           static_assert(!std::is_same_v<T,T>,"Unsupport Type");
+    }
+};
+namespace {
+    template <typename ... restAttrs>
+    inline void Draw_tuple_element(std::tuple<restAttrs...>&rest)
+    {
+        
+    }
+}
+template <Visible_Attr_Group_Type T>
+inline void Draw_element(T &t)
+{
+    auto &data = t.data;
+    if (ImGui::CollapsingHeader(t.GetName(), t.GetFlag()))
+    {
+        auto &attrs=t.GetAttrs();
+        Draw_tuple_element(attrs);
+    }
+}
--- src/test_frame/test_frame_app.cpp
+++ src/test_frame/test_frame_app.cpp
@@ -1,95 +1,177 @@
-#include "imgui.h"
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
-#include <stdio.h>
-#include <Windows.h>
-#include <tchar.h>
-#ifdef __APPLE__
-#define GL_SILENCE_DEPRECATION
-#endif
-#include <GLFW/glfw3.h>
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
-#endif
+#include <helper/OpenGL_Utils.h>
+#include <helper/OpenGL_Objects.h>
+#include <ImGUI/ImGUI_Utils.h>
+#include <cmath>
 
-
-
-int  tmain()
+Universal_Type_Wrapper<int> ui("test int", 1, 1, 99, 10);
+Universal_Type_Wrapper<float> uf("test float", 1, 1, 99);
+int unwrapped_i;
+float unwrapped_f;
+Universal_Type_Wrapper<int&> wi("wrapped int",std::ref( unwrapped_i),1,99);
+Universal_Type_Wrapper<float&> wf("wrapped float",std::ref( unwrapped_f),1,1,99);
+static void glfw_error_callback(int error, const char *description)
 {
-	
-	if (!glfwInit())   //初始化OpenGl
+	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+constexpr int window_width = 1440, window_height = 900;
+using namespace std::string_literals;
+
+int tmain()
+{
+	// Setup window
+	glfwSetErrorCallback(glfw_error_callback);
+	if (!glfwInit())
 		return 1;
+	// Decide GL+GLSL versions
+	const char *glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-	//创建OpenGl窗口
-
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL2 example", NULL, NULL);
+	// Create window with graphics context
+	GLFWwindow *window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
 	if (window == NULL)
 		return 1;
+	Light::OpenGLContext context = window;
+	context.init();
 
-	//设置OpenGl山下文
-	glfwMakeContextCurrent(window);
+	glViewport(0, 0, window_width / 2, window_height / 2);
+
+	// clang-format off
+	ProgramObject program = Helper::CreateProgram(
+		ShaderObject(GL_VERTEX_SHADER, readFile("../media/shaders/"s + "quick_use_simple" + "/this.vs.glsl")),
+		ShaderObject(GL_FRAGMENT_SHADER, readFile("../media/shaders/"s +"quick_use_simple" + "/this.fs.glsl"))
+		);
+
+	constexpr float L=0.8,R=-L;
+	constexpr float TL=0,TR=1;
+
+	float vertices[]={
+		L,L,0,TL,TL,
+		L,R,0,TL,TR,
+		R,R,0,TR,TR,
+		R,L,0,TR,TL,
+	};
+	uint32_t indices[]={
+		0,1,2,
+		0,2,3,
+	};
+	Light::BufferLayout layout = {
+		Light::BufferElement(Light::ShaderDataType::Float3, "aPos", false),
+		Light::BufferElement(Light::ShaderDataType::Float2, "aTexCoord", false)
+		};
+	// clang-format on
+	auto vao = std::shared_ptr<Light::VertexArray>(Light::VertexArray::create());
+	auto vbo = std::shared_ptr<Light::VertexBuffer>(Light::VertexBuffer::create(vertices, sizeof(vertices)));
+	auto veo = std::shared_ptr<Light::IndexBuffer>(Light::IndexBuffer::create(indices, sizeof(indices)));
+
+	vbo->setLayout(layout);
+	vao->addVertexBuffer(vbo);
+	vao->setIndexBuffer(veo);
+
+	auto texture = Helper::CreateTexture(GL_TEXTURE0, "../media/texture/bmp/2004050204170.bmp");
+	glUseProgram(program.getProgram());
+	program.setInt("aTexture", 0);
+
+	// set attribute location
+	program.prepareVBO(*vbo.get());
+	glClearColor(0.2, 0.2, 0.0, 1);
+
 	glfwSwapInterval(1); // Enable vsync
 
-	// 设置ImGui舌下文.
+	// Setup Dear ImGui context
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	
+	ImGuiIO &io = ImGui::GetIO();
+	(void)io;
 
-	//设置颜色风格
 	ImGui::StyleColorsDark();
-	
 
-	// Setup Platform/Renderer bindings
+	// Setup Platform/Renderer backends
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init();
+	ImGui_ImplOpenGL3_Init(glsl_version);
 
+	// Our state
+	bool show_demo_window = true;
+	bool show_another_window = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 	// Main loop
 	while (!glfwWindowShouldClose(window))
 	{
-		
+
+		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		vao->bind();
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (GLuint *)(0) + 3);
+		vao->unbind();
+		// Poll and handle events (inputs, window resize, etc.)
+		// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+		// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+		// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+		// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
 		glfwPollEvents();
 
-		// Start the Dear ImGui frame 启动IMgui Frame框架.
+		// Start the Dear ImGui frame
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		
+		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+
+		static float f = 0.0f;
+		static int counter = 0;
+		using namespace ImGui;
+		bool show_crash_window = true;
+		static float fs[10] = {
+			0,
+		};
+		Begin("test");
+		if (ImGui::CollapsingHeader(":test header"))
 		{
-			//开始绘制ImGui
-			
-			ImGui::Begin("IBinary Windows");                          // Create a window called "Hello, world!" and append into it.
-			ImGui::Text("IBinary Blog");
-			//ImGui::SameLine();
-			ImGui::Indent(); //另起一行制表符开始绘制Button
-			ImGui::Button("2222", ImVec2(100, 50));
-			
-		
+			BeginTabBar("Sad");
+			if (BeginTabItem("b"))
+			{
+				using UT=decltype(ui);
+				using T=UT::ValueType;
+				using DT=UT::DT;
+				constexpr bool x=std::is_same_v<DT,int>;
+				auto constexpr xx=Visible_Attr_Type<UT>;
+				static_assert(x);
+				Draw_element(ui);
+				Draw_element(uf);
+				Draw_element(wi);
+				Draw_element(wf);
+
+				EndTabItem();
+			}
+			EndTabBar();
+		}
+		End();
+		// 3. Show another simple window.
+		if (show_another_window)
+		{
+			ImGui::Begin("Another Window", &show_another_window); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+			ImGui::Text("Hello from another window!");
+			if (ImGui::Button("Close Me"))
+				show_another_window = false;
 			ImGui::End();
 		}
-
-		// 3. Show another simple window.
-		
 		// Rendering
 		ImGui::Render();
-
-		
 		int display_w, display_h;
 		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-		glClear(GL_COLOR_BUFFER_BIT);
-		
-		
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData()); //必须在绘制完Open之后接着绘制Imgui
-		//glUseProgram(last_program);
+		glViewport(0, 0, display_w / 2, display_h / 2);
+		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		glfwMakeContextCurrent(window);
-		glfwSwapBuffers(window);
+		// glClear(GL_COLOR_BUFFER_BIT);
+		//  enable shutdown
+		context.swapBuffers();
 	}
 
 	// Cleanup
@@ -103,8 +185,12 @@ int  tmain()
 	return 0;
 }
 
-
 #include <gtest/gtest.h>
-TEST(l,r){
-    tmain();
+TEST(l, r)
+{
+	constexpr bool iss[] = {
+		Visible_Attr_Type<Universal_Type_Wrapper<int>>,
+		// Visible_Attr_Type<Universal_Type_Wrapper<float>>
+	};
+	tmain();
 }
